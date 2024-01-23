9fae9b5aab064c6925070f395467a3ef211ec2d4
EdmunnTB <126532963+EdmunnTB@users.noreply.github.com>
2024-01-23 02:50:21 -0500

    Top-Down Controls
    -added purple guy sprite & animations (subject to change)
    -added character sprite flipping

--- Eddy OVDR Game/Assets/Scripts/PlayerController.cs
+++ Eddy OVDR Game/Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -8,26 +9,69 @@ public class PlayerController : MonoBehaviour
     float verticalInput;
     public float speed = 8f;
     public float jumpPower = 8f;
-    [SerializeField]private Rigidbody2D body;
+    private bool facingRight;
+    [SerializeField] private Rigidbody2D body;
+    [SerializeField] private GameObject player;
+    [SerializeField] private Animator anim;
+
 
     void Start()
     {
- 
+        anim = GetComponent<Animator>();
     }
     
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
-        
+        FlipLogic();
+        Animate();
     }
 
     void FixedUpdate()
     {
-        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
-        if (verticalInput == 1)
+        MovePlayer();
+    }
+    
+    private void MovePlayer()
+    {
+        Vector2 playerInput = new Vector2(horizontalInput, verticalInput);
+        Vector2 moveForce = playerInput * speed;
+        body.velocity = moveForce;
+    }
+
+    private void Flip()
+    {
+        // Switch the way the player is labelled as facing
+        facingRight = !facingRight;
+        player.transform.Rotate(0, 180, 0);
+    }
+
+    private void FlipLogic()
+    {
+        if (horizontalInput > 0f && !facingRight)
         {
-            body.velocity = new Vector2(body.velocity.x, verticalInput * jumpPower);
+            //If we're moving right but not facing right, flip the sprite and set     facingRight to true.
+            Flip();
+            facingRight = true;
+        }
+        else if (horizontalInput < 0f && facingRight)
+        {
+            //If we're moving left but not facing left, flip the sprite and set facingRight to false.
+            Flip();
+            facingRight = false;
+        }
+    }
+
+    private void Animate()
+    {
+        if (horizontalInput != 0)
+        {
+            anim.SetBool("HorizontalMove", true);
+        }
+        else
+        {
+            anim.SetBool("HorizontalMove", false);
         }
     }
 }
